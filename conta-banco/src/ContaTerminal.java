936f8a3d8dc0adc5c2303a3f320e4d2247a6c3a1
DESKTOP-KKPA9T7\izabe <izabel.c.azoia@gmail.com>
2024-06-06 17:43:50 -0300

    segundo commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+
+        System.out.println("Digite o número da conta");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite sua agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        
+        System.out.println("O numero da Conta é " + numero );
+        System.out.println("Sua agencia é " + agencia );
+        System.out.println("Nome do Cliente " + nome + " " + sobrenome);
+        System.out.println("Seu saldo é " + saldo );
     }
 }
